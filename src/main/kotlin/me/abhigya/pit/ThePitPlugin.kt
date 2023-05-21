5e81da6d78e664970b178984298ecda39a594ffe
Abhigya <abhigyakrishna21@gmail.com>
2023-05-22 01:30:42 +0530

    Compatibility setup update

--- src/main/kotlin/me/abhigya/pit/ThePitPlugin.kt
+++ src/main/kotlin/me/abhigya/pit/ThePitPlugin.kt
@@ -16,8 +16,10 @@ import me.abhigya.pit.util.Platform
 import net.kyori.adventure.platform.AudienceProvider
 import net.kyori.adventure.platform.bukkit.BukkitAudiences
 import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.Bukkit
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
+import org.spongepowered.configurate.ScopedConfigurationNode
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader
 import toothpick.Scope
 import toothpick.configuration.Configuration
@@ -35,6 +37,21 @@ class ThePitPlugin : JavaPlugin(), CoroutineScope by CoroutineScope(
 ) {
 
     override fun onEnable() {
+        logger.log(Level.INFO, "Running compatibility setup...")
+        runCatching {
+            if (runCompatibilitySetup()) {
+                logger.log(Level.INFO, "Compatibility setup success. Restarting server...")
+                this.server.shutdown()
+                return
+            } else {
+                logger.log(Level.INFO, "Compatibility up to date!")
+            }
+        }.onFailure {
+            logger.log(Level.SEVERE, "Compatibility setup failed!", it)
+            this.server.pluginManager.disablePlugin(this)
+            return
+        }
+
         KTP.setConfiguration(Configuration.forDevelopment())
         val scope = KTP.openScope(this::class.java) {
             it.installModules(
@@ -97,18 +114,46 @@ class ThePitPlugin : JavaPlugin(), CoroutineScope by CoroutineScope(
         KTP.closeScope(this::class.java)
     }
 
-    fun runCompatibilitySetup() {
-        if (Platform.CURRENT == Platform.PAPER) {
-            val paperCfg = File("paper.yml")
-            if (paperCfg.exists()) {
-                YamlConfigurationLoader.builder()
-                    .file(paperCfg)
-                    .build().let {
-                        val cfg = it.load()
-                        cfg.node("warnWhenSettingExcessiveVelocity").set(false)
-                        it.save(cfg)
+    private fun runCompatibilitySetup(): Boolean {
+        var isDirty = false
+
+        fun patchConfig(file: File, vararg patches: Triple<String, (ScopedConfigurationNode<*>) -> Boolean, Any>) {
+            YamlConfigurationLoader.builder()
+                .file(file)
+                .build().run {
+                    val cfg = this.load()
+                    var localDirty = false
+                    for (patch in patches) {
+                        val node = cfg.node(patch.first.split("."))
+                        if (patch.second(node)) {
+                            node.set(patch.third)
+                            isDirty = true
+                            localDirty = true
+                        }
                     }
+
+                    if (localDirty) this.save(cfg)
+                }
+        }
+
+        if (Platform.CURRENT == Platform.PAPER) {
+            File("paper.yml").runCatching {
+                patchConfig(this, Triple("warnWhenSettingExcessiveVelocity", { it.getBoolean(true) }, false))
+            }.onFailure {
+                throw CompatibilitySetupException("Failed to patch paper.yml", it)
             }
         }
+
+        if (Bukkit.getPluginManager().getPlugin("ViaVersion") != null) {
+            File("plugins/ViaVersion/config.yml").runCatching {
+                patchConfig(this, Triple("use-1_15-instant-respawn", { !it.boolean }, true))
+            }.onFailure {
+                throw CompatibilitySetupException("Failed to patch ViaVersion config.yml", it)
+            }
+        }
+
+        return isDirty
     }
+
+    class CompatibilitySetupException(message: String, cause: Throwable? = null) : Exception(message, cause)
 }
